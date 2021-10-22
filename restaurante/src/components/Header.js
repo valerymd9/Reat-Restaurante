b08093871562ea0d35616bbed4d1796e04955555
valerymd9 <91355386+valerymd9@users.noreply.github.com>
2021-10-21 23:23:47 -0500

    Componente Header

---
+++ restaurante/src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+export default class Header extends React.Component {
+
+    render() {
+
+        return (
+
+            <
+            div > < /div>
+
+        )
+
+    }
+
+
+
+}
