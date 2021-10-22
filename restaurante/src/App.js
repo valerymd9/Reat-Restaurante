a6f0a34e15ee3a25d6f8b7f379016ceaa6023ab9
valerymd9 <91355386+valerymd9@users.noreply.github.com>
2021-10-22 11:32:13 -0500

    header

--- restaurante/src/App.js
+++ restaurante/src/App.js
@@ -1,18 +1,15 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Header from './components/Header.js'
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        header >
-        <
-        Header / >
-        <
-        /header> <
-        /div>
-    );
+  return (
+    <div className="App">
+      <header>
+      <Header/>
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- restaurante/src/components/Header.js
+++ restaurante/src/components/Header.js
@@ -6,23 +6,19 @@ export default class Header extends React.Component {
     render() {
 
         return (
+         
 
-
-            <
-            Navbar className = "navbar1" >
-            <
-            Container >
-            <
-            Navbar.Brand href = "#home" > Restaurante D 'Gusto</Navbar.Brand> <
-            Nav className = "me-auto my-2 my-lg-0" >
-            <
-            Nav.Link href = "#home" > Home < /Nav.Link> <
-            Nav.Link href = "#features" > Quienes somos < /Nav.Link> <
-            Nav.Link href = "#pricing" > Menu < /Nav.Link> <
-            Nav.Link href = "#contact" > Contactanos < /Nav.Link> <
-            /Nav> <
-            /Container> <
-            /Navbar>
+          <Navbar className="navbar1">
+    <Container>
+    <Navbar.Brand href="#home">Restaurante D'Gusto</Navbar.Brand>
+    <Nav className="me-auto my-2 my-lg-0">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Quienes somos</Nav.Link>
+      <Nav.Link href="#pricing">Menu</Nav.Link>
+      <Nav.Link href="#contact">Contactanos</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
         )
 
     }
