f6fb75777b3a492aade006940f314b1b000f2a71
valerymd9 <91355386+valerymd9@users.noreply.github.com>
2021-10-27 11:16:55 -0500

    Cambios Valery

---
+++ src/components/Footer.js
@@ -0,0 +1,64 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+
+export default class Footer extends React.Component {
+
+    render() {
+
+        return (
+
+          <div className="main-footer">
+          <div className="container">
+            <div className="row">
+              {/* Column1 */}
+              <div className="col">
+                <h4>Menú</h4>
+                <ui className="list-unstyled">
+                  <li>342-420-6969</li>
+                  <li>Moscow, Russia</li>
+                  <li>123 Streeet South North</li>
+                </ui>
+              </div>
+              {/* Column2 */}
+              <div className="col">
+                <h4>Servicios</h4>
+                <ui className="list-unstyled">
+                  <li>DANK MEMES</li>
+                  <li>OTHER STUFF</li>
+                  <li>GUD STUFF</li>
+                </ui>
+              </div>
+              {/* Column3 */}
+              <div className="col">
+                <h4>Contactanos</h4>
+                <ui className="list-unstyled">
+                  <li>DANK MEMES</li>
+                  <li>OTHER STUFF</li>
+                  <li>GUD STUFF</li>
+                </ui>
+              </div>
+                {/* Column4 */}
+              <div className="col">
+                <h4>Redes Sociales</h4>
+                <ui className="list-unstyled">
+                  <li>DANK MEMES</li>
+                  <li>OTHER STUFF</li>
+                  <li>GUD STUFF</li>
+                </ui>
+              </div>
+          </div>
+            <hr />
+            <div className="row">
+              <p className="col-sm">
+                &copy;{new Date().getFullYear()}| All rights reserved |
+                Terms Of Service | Privacy
+              </p>
+            </div>
+          </div>
+        </div>
+                );
+              
+
+   }   }
---
+++ src/components/Header.js
@@ -0,0 +1,28 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import { Navbar, Nav, Container, } from 'react-bootstrap';
+export default class Header extends React.Component {
+
+    render() {
+
+        return (
+         
+
+          <Navbar className="navbar1">
+    <Container>
+    <Navbar.Brand href="#home">Restaurante D'Gusto</Navbar.Brand>
+    <Nav className="me-auto my-2 my-lg-0">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Quienes somos</Nav.Link>
+      <Nav.Link href="#pricing">Menu</Nav.Link>
+      <Nav.Link href="#contact">Contactanos</Nav.Link>
+    </Nav>
+    </Container>
+  </Navbar>
+        )
+
+    }
+
+
+
+}
