126e0ca50238778ae258f294c8330e2e60f600f7
valerymd9 <91355386+valerymd9@users.noreply.github.com>
2021-10-29 22:51:26 -0500

    datamenu

---
+++ restaurante/src/components/menu/menu.js
@@ -0,0 +1,91 @@
+export const menuData = [
+
+    {
+        id: 1,
+        name: "Bandeja Paisa",
+        description: "",
+        price: 33000,
+        image: "./images/bandeja_pais.jpg",
+    },
+    {
+        id: 2,
+        name: "Mojarra Frita",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, sunt eos. Unde!",
+        price: 20000,
+        image: "./images/mojarra.jpg",
+    },
+    {
+        id: 3,
+        name: "Pepitorio",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, sunt eos. Unde!",
+        price: 25000,
+        image: "./images/pepitoria.jpg",
+    },
+
+    {
+        id: 4,
+        name: "Mute",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, sunt eos. Unde!",
+        price: 25000,
+        image: "./images/mute.gif",
+    },
+    {
+        id: 5,
+        name: "Sancocho",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, sunt eos. Unde!",
+        price: 25000,
+        image: "./images/Sancocho.jpg",
+    },
+    {
+        id: 6,
+        name: "Ajiaco con Pollo",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, sunt eos. Unde!",
+        price: 25000,
+        image: "./images/AJIACO.jpg",
+    },
+
+
+    {
+        id: 7,
+        name: "Lechona",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, sunt eos. Unde!",
+        price: 25000,
+        image: "./images/LECHONA.jpg",
+    },
+    {
+        id: 8,
+        name: "Carne Oreada",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, sunt eos. Unde!",
+        price: 25000,
+        image: "./images/carne.jpg",
+    },
+    {
+        id: 9,
+        name: "Sushi",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, sunt eos. Unde!",
+        price: 25000,
+        image: "sushi.png",
+    },
+
+    {
+        id: 10,
+        name: "Ensalada César",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, sunt eos. Unde!",
+        price: 25000,
+        image: "ensalada_cesar.png",
+    },
+    {
+        id: 11,
+        name: "Ens",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, sunt eos. Unde!",
+        price: 25000,
+        image: "./images/cazuela.jpg",
+    },
+    {
+        id: 12,
+        name: "Camarones",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, sunt eos. Unde!",
+        price: 25000,
+        image: "./images/camarones.jpg",
+    }
+]
