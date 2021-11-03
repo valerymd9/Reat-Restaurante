03fc18d70d9a7a7707cbd4c7761f077d8cb3160f
valerymd9 <91355386+valerymd9@users.noreply.github.com>
2021-11-03 10:21:25 -0500

    update testimonios

--- restaurante/src/components/testimonioUsuario.js
+++ restaurante/src/components/testimonioUsuario.js
@@ -6,39 +6,58 @@ import testimonio from '../data/testimoniosUsuarios.json';
 
 export default class TestimonioU extends React.Component {
     render() {
-        let testimonioData = testimonio ;
-        return (
-            <section class="client_section">
-            <div class="container">
-              <h2 class="custom_heading">Testimonios</h2>
-              <p class="custom_heading-text">
-                Testimonios de clientes de los ultimos seis meses:
-              </p>
-              <div>
-                <div id="carouselExampleControls-2" class="carousel slide">
-                  <div class="carousel-inner">
-                    <div class="carousel-item active">
-                      <div class="client_container layout_padding2">
+        let testimonioData = testimonio;
+        return ( <
+            section class = "client_section" >
+            <
+            div class = "container" >
+            <
+            h1 class = "custom_heading" > TESTIMONIOS < /h1> <
+            p class = "custom_heading-text" >
+            Testimonios de clientes de los ultimos seis meses:
+            <
+            /p> <
+            div >
+            <
+            div id = "carouselExampleControls-2"
+            class = "carousel slide" >
+            <
+            div class = "carousel-inner" >
+            <
+            div class = "carousel-item active" >
+            <
+            div class = "client_container layout_padding2" >
 
-                      {testimonioData.map(data =>
-                            <Testimonio id={data.id} image = {data.image} nameU = {data.nameU} comentario = {data.comentario}></Testimonio>
-                        )} 
-                      </div>
-            </div>
-            <div class="custom_carousel-control">
-            <a class="carousel-control-prev" href="#carouselExampleControls-2">
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleControls-2">
-              <span class=""></span>
-              <span class="sr-only">Siguiente</span>
-            </a>
-          </div>
-            </div>
-      </div>
-    </div>
-    </div>
-  </section>
+            {
+                testimonioData.map(data =>
+                    <
+                    Testimonio id = { data.id }
+                    image = { data.image }
+                    nameU = { data.nameU }
+                    comentario = { data.comentario } > < /Testimonio>
+                )
+            } <
+            /div> <
+            /div> <
+            div class = "custom_carousel-control" >
+            <
+            a class = "carousel-control-prev"
+            href = "#carouselExampleControls-2" >
+            <
+            span class = "sr-only" > Previous < /span> <
+            /a> <
+            a class = "carousel-control-next"
+            href = "#carouselExampleControls-2" >
+            <
+            span class = "" > < /span> <
+            span class = "sr-only" > Siguiente < /span> <
+            /a> <
+            /div> <
+            /div> <
+            /div> <
+            /div> <
+            /div> <
+            /section>
 
         );
 
--- restaurante/src/components/testimonios.js
+++ restaurante/src/components/testimonios.js
@@ -1,16 +1,26 @@
 import './styles/testimonios.css';
 export const Testimonio = (props) => {
 
-    return (
-        <>
-            <div align="center" id={props.id} >
-            <div class="client_img-box">
-                <img src={props.image} alt=""  width="140 " height="140 " />
-              </div>
-              <div class="client_detail">
-                <h3>{props.nameU}</h3></div>
-                <p class="custom_heading-text">{props.comentario}</p>
-            </div>
-        </>
+    return ( <
+        >
+        <
+        div align = "center"
+        id = { props.id } >
+        <
+        div class = "client_img-box" >
+        <
+        img src = { props.image }
+        alt = ""
+        width = "140 "
+        height = "140 " / >
+        <
+        /div> <
+        div class = "client_detail" >
+        <
+        h3 > { props.nameU } < /h3></div >
+        <
+        p class = "custom_heading-text" > { props.comentario } < /p> <
+        /div> <
+        />
     );
 }
