24b22f7acd71080e8bf35c55422d11f15c972e06
valerymd9 <91355386+valerymd9@users.noreply.github.com>
2021-11-04 10:21:57 -0500

    Create inicio.jsx

---
+++ restaurante/src/components/inicio.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+
+export default class Inicio extends React.Component {
+
+    render() {
+
+        return (
+<div>Home</div>
+        );
+    }
+}
